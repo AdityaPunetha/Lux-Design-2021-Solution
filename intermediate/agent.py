d7b891ca16928fe52c140bba00fbd0e5492aaeff
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2021-10-14 19:32:28 +0530

    Incorporated multiple cities, workers and research system

--- intermediate/agent.py
+++ intermediate/agent.py
@@ -58,10 +58,10 @@ def find_empty_tile_near(near_what, game_state, observation):
         try:
             possible_empty_tile = game_state.map.get_cell(near_what.pos.x + d[0],
                                                           near_what.pos.y + d[1])
-            logging.info(f"{observation['step']}: checking: {possible_empty_tile}")
+            # logging.info(f"{observation['step']}: checking: {possible_empty_tile}")
             if possible_empty_tile.resource is None and possible_empty_tile.road == 0 and possible_empty_tile.citytile is None:
                 build_location = possible_empty_tile
-                logging.info(f"{observation['step']}: Found: {possible_empty_tile}")
+                # logging.info(f"{observation['step']}: Found: {possible_empty_tile}")
                 return build_location
         except Exception as e:
             logging.warning(f"{observation['step']}: while search: {str(e)}")
@@ -70,10 +70,10 @@ def find_empty_tile_near(near_what, game_state, observation):
         try:
             possible_empty_tile = game_state.map.get_cell(near_what.pos.x + d[0],
                                                           near_what.pos.y + d[1])
-            logging.info(f"{observation['step']}: checking: {possible_empty_tile}")
+            # logging.info(f"{observation['step']}: checking: {possible_empty_tile}")
             if possible_empty_tile.resource is None and possible_empty_tile.road == 0 and possible_empty_tile.citytile is None:
                 build_location = possible_empty_tile
-                logging.info(f"{observation['step']}: Found: {possible_empty_tile}")
+                # logging.info(f"{observation['step']}: Found: {possible_empty_tile}")
                 return build_location
         except Exception as e:
             logging.warning(f"{observation['step']}: while search: {str(e)}")
@@ -110,14 +110,14 @@ def agent(observation, configuration):
             logging.info(f"Found worker w/o resource {w.id}")
             resource_assignment = get_close_resource(w, resource_tiles, player)
             unit_to_resource_dict[w.id] = resource_assignment
-    logging.info(f" worker :{workers}")
+    # logging.info(f" worker :{workers}")
     cities = player.cities.values()
     city_tiles = []
     for city in cities:
         for c_tile in city.citytiles:
             city_tiles.append(c_tile)
-    logging.info(f"{cities}")
-    logging.info(f"{city_tiles}")
+    # logging.info(f"{cities}")
+    # logging.info(f"{city_tiles}")
     build_city = False
     if len(workers) / len(city_tiles) >= 0.75:
         build_city = True
@@ -131,58 +131,79 @@ def agent(observation, configuration):
                     actions.append(unit.move(unit.pos.direction_to(intended_resource.pos)))
                 else:
                     intended_resource = get_close_resource(unit, resource_tiles, player)
-                    unit_to_resource_dict[unit.id] = resource_assignment
+                    unit_to_resource_dict[unit.id] = intended_resource
                     actions.append(unit.move(unit.pos.direction_to(intended_resource.pos)))
             else:
                 if build_city:
-                    if build_location is None:
-                        empty_near = get_close_city(player, unit)
-                        build_location = find_empty_tile_near(empty_near, game_state, observation)
-                    if unit.pos == build_location.pos:
-                        action = unit.build_city()
-                        actions.append(action)
-                        build_city = False
-                        build_location = None
-                        continue
-                    else:
-                        logging.info(f"{observation['step']}: Navigating:")
-                        # actions.append(unit.move(unit.pos.direction_to(build_location.pos)))
-                        dir_diff = (build_location.pos.x - unit.pos.x, build_location.pos.y - unit.pos.y)
-                        xdiff, ydiff = dir_diff
-                        if abs(ydiff) > abs(xdiff):
-                            check_tile = game_state.map.get_cell(unit.pos.x, unit.pos.y + np.sign(ydiff))
-                            if check_tile.citytile is None:
-                                if np.sign(ydiff) == 1:
-                                    actions.append(unit.move('s'))
-                                else:
-                                    actions.append(unit.move('n'))
-                            else:
-                                if np.sign(xdiff) == 1:
-                                    actions.append(unit.move('e'))
-                                else:
-                                    actions.append(unit.move('w'))
+                    associated_city_id = unit_to_city_dict[unit.id].cityid
+                    unit_city = [c for c in cities if c.cityid == associated_city_id][0]
+                    unit_city_fuel = unit_city.fuel
+                    unit_city_size = len(unit_city.citytiles)
+                    enough_fuel = (unit_city_fuel / unit_city_size) > 300
+                    logging.info(
+                        f"{observation['step']}: Built the city: {associated_city_id}, fule {unit_city_fuel}, size {unit_city_size}, en {enough_fuel}")
+                    if enough_fuel:
+                        if build_location is None:
+                            empty_near = get_close_city(player, unit)
+                            build_location = find_empty_tile_near(empty_near, game_state, observation)
+                        if unit.pos == build_location.pos:
+                            action = unit.build_city()
+                            actions.append(action)
+                            build_city = False
+                            build_location = None
+                            logging.info(f"{observation['step']}: Built the city:")
+
+                            continue
                         else:
-                            check_tile = game_state.map.get_cell(unit.pos.x + np.sign(xdiff), unit.pos.y)
-                            if check_tile.citytile is None:
-                                if np.sign(xdiff) == 1:
-                                    actions.append(unit.move('e'))
+                            logging.info(f"{observation['step']}: Navigating:")
+                            # actions.append(unit.move(unit.pos.direction_to(build_location.pos)))
+                            dir_diff = (build_location.pos.x - unit.pos.x, build_location.pos.y - unit.pos.y)
+                            xdiff, ydiff = dir_diff
+                            if abs(ydiff) > abs(xdiff):
+                                check_tile = game_state.map.get_cell(unit.pos.x, unit.pos.y + np.sign(ydiff))
+                                if check_tile.citytile is None:
+                                    if np.sign(ydiff) == 1:
+                                        actions.append(unit.move('s'))
+                                    else:
+                                        actions.append(unit.move('n'))
                                 else:
-                                    actions.append(unit.move('w'))
+                                    if np.sign(xdiff) == 1:
+                                        actions.append(unit.move('e'))
+                                    else:
+                                        actions.append(unit.move('w'))
                             else:
-                                if np.sign(ydiff) == 1:
-                                    actions.append(unit.move('s'))
+                                check_tile = game_state.map.get_cell(unit.pos.x + np.sign(xdiff), unit.pos.y)
+                                if check_tile.citytile is None:
+                                    if np.sign(xdiff) == 1:
+                                        actions.append(unit.move('e'))
+                                    else:
+                                        actions.append(unit.move('w'))
                                 else:
-                                    actions.append(unit.move('n'))
-                        continue
+                                    if np.sign(ydiff) == 1:
+                                        actions.append(unit.move('s'))
+                                    else:
+                                        actions.append(unit.move('n'))
+                            continue
+                    elif len(player.cities) > 0:
+                        if unit.id in unit_to_city_dict and unit_to_city_dict[unit.id] in city_tiles:
+                            move_dir = unit.pos.direction_to(unit_to_city_dict[unit.id].pos)
+                            actions.append(unit.move(move_dir))
+                        else:
+                            unit_to_city_dict[unit.id] = get_close_city(player, unit)
+                            move_dir = unit.pos.direction_to(unit_to_city_dict[unit.id].pos)
+                            actions.append(unit.move(move_dir))
                 # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
-                elif len(player.cities) > 0:
-                    if unit.id in unit_to_city_dict and unit_to_city_dict[unit.id] in city_tiles:
-                        move_dir = unit.pos.direction_to(unit_to_city_dict[unit.id].pos)
-                        actions.append(unit.move(move_dir))
-                    else:
-                        unit_to_city_dict[unit.id] = get_close_city(player, unit)
-                        move_dir = unit.pos.direction_to(unit_to_city_dict[unit.id].pos)
-                        actions.append(unit.move(move_dir))
-    # you can add debug annotations using the functions in the annotate object
-    # actions.append(annotate.circle(0, 0))
+
+    can_create = len(city_tiles) - len(workers)
+    if len(city_tiles) > 0:
+        for city_tile in city_tiles:
+            if city_tile.can_act():
+                if can_create > 0:
+                    actions.append(city_tile.build_worker())
+                    can_create -= 1
+                    logging.info(f"{observation['step']}: Created worker")
+                else:
+                    actions.append(city_tile.research())
+                    logging.info(f"{observation['step']}: Started Research")
+
     return actions
